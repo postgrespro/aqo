62cddbcad229608877d9b18c8ab6ac8157f0ef71
Andrey V. Lepikhov <a.lepikhov@postgrespro.ru>
2021-07-10 10:35:03 +0300

    Use the aqo_get_clauses routine to get restrictinfos. This routine does a copy od each clause and changes SubPlan clauses with the hash value corresponding to this SubPlan. So we get in account subplans. On the learning stage we plunge into a subplan (for learning on underlying nodes) but don't get into account its clauses, relids and selectivities.
    Regression tests on this feature added. Still in the unsupported.sql.

--- sql/unsupported.sql
+++ sql/unsupported.sql
@@ -23,5 +23,42 @@ EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF, TIMING OFF)
 	SELECT count(*) FROM t WHERE x = (
 		SELECT avg(x) FROM t t0 WHERE t0.x = t.x
 	);
-	
+
+-- Two identical subplans in a clause list
+EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF, TIMING OFF)
+	SELECT count(*) FROM t WHERE
+		x = (SELECT avg(x) FROM t t0 WHERE t0.x = t.x + 21) OR
+		x IN (SELECT avg(x) FROM t t0 WHERE t0.x = t.x + 21);
+EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF, TIMING OFF)
+	SELECT count(*) FROM t WHERE
+		x = (SELECT avg(x) FROM t t0 WHERE t0.x = t.x + 21) OR
+		x IN (SELECT avg(x) FROM t t0 WHERE t0.x = t.x + 21);
+
+-- It's OK to use the knowledge for a query with different constants.
+EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF, TIMING OFF)
+	SELECT count(*) FROM t WHERE
+		x = (SELECT avg(x) FROM t t0 WHERE t0.x = t.x + 22) OR
+		x IN (SELECT avg(x) FROM t t0 WHERE t0.x = t.x + 23);
+
+-- Different SubPlans in the quals of leafs of JOIN.
+SELECT count(*) FROM
+	(SELECT * FROM t WHERE x % 3 < (SELECT avg(x) FROM t t0 WHERE t0.x = t.x)) AS q1
+		JOIN
+	(SELECT * FROM t WHERE x % 3 < (SELECT avg(x) FROM t t0 WHERE t0.x <> t.x)) AS q2
+		ON q1.x = q2.x+1;
+EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF, TIMING OFF)
+SELECT count(*) FROM
+	(SELECT * FROM t WHERE x % 3 < (SELECT avg(x) FROM t t0 WHERE t0.x = t.x)) AS q1
+		JOIN
+	(SELECT * FROM t WHERE x % 3 < (SELECT avg(x) FROM t t0 WHERE t0.x <> t.x)) AS q2
+		ON q1.x = q2.x+1;
+
+-- Two identical subplans in a clause
+EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF, TIMING OFF)
+	SELECT count(*) FROM t WHERE (SELECT avg(x) FROM t t0 WHERE t0.x = t.x) =
+	(SELECT avg(x) FROM t t0 WHERE t0.x = t.x);
+EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF, TIMING OFF)
+	SELECT count(*) FROM t WHERE (SELECT avg(x) FROM t t0 WHERE t0.x = t.x) =
+	(SELECT avg(x) FROM t t0 WHERE t0.x = t.x);
+
 DROP EXTENSION aqo;
