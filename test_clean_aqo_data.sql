09383143dad5602ba8909ed36d081d95f66d97f4
Pervushina Alexandra <a.pervushina@postgrespro.ru>
2021-07-16 05:39:11 +0300

    Added clean_aqo_data function

---
+++ test_clean_aqo_data.sql
@@ -0,0 +1,25 @@
+CREATE EXTENSION aqo;
+
+CREATE TABLE a();
+SELECT * FROM a;
+SELECT oids FROM aqo_data;
+SELECT clean_aqo_data();
+-- в aqo_data не должна пропасть строка с oid a
+SELECT oids FROM aqo_data;
+
+DROP TABLE a;
+SELECT clean_aqo_data();
+-- в aqo_data должна исчезнуть запись
+SELECT oids FROM aqo_data;
+
+CREATE TABLE a();
+CREATE TABLE b();
+SELECT * FROM a;
+SELECT * FROM b;
+SELECT * FROM a CROSS JOIN b;
+-- в aqo_data появились 3 записи
+SELECT oids FROM aqo_data;
+DROP TABLE a;
+SELECT clean_aqo_data();
+-- в aqo_data исчезли 2 записи
+SELECT oids FROM aqo_data;
