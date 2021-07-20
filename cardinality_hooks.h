b3a669cbe3ad6b0b7882abee3f6e17622d2df9aa
Andrey V. Lepikhov <a.lepikhov@postgrespro.ru>
2021-07-20 10:38:06 +0500

    Add a hook (and AQO function for the interception) for a number of groups estimation.
    Core Patch was changed.

---
+++ cardinality_hooks.h
@@ -0,0 +1,13 @@
+#ifndef CARDINALITY_HOOKS_H
+#define CARDINALITY_HOOKS_H
+
+#include "optimizer/planner.h"
+#include "utils/selfuncs.h"
+
+extern estimate_num_groups_hook_type prev_estimate_num_groups_hook;
+
+extern double aqo_estimate_num_groups_hook(PlannerInfo *root, List *groupExprs,
+										   RelOptInfo *rel, List **pgset,
+										   EstimationInfo *estinfo);
+
+#endif /* CARDINALITY_HOOKS_H */
