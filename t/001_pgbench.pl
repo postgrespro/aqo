d9efdbcf65c985933bf3f3132d8e39d14aaab411
Andrey Lepikhov <a.lepikhov@postgrespro.ru>
2021-09-17 09:04:23 +0500

    Fix into the pgbench TAP test: reduce number of clients and threads during the stress test on a postgres instance with installed AQO extension. This is enough to check correctness of concurrent access to a ML knowledge base.

--- t/001_pgbench.pl
+++ t/001_pgbench.pl
@@ -13,8 +13,6 @@ $node->append_conf('postgresql.conf', qq{
 						aqo.log_ignorance = 'on'
 					});
 
-#my $result1;
-
 $node->start();
 
 # Check conflicts of accessing to the ML knowledge base
@@ -22,26 +20,26 @@ $node->start();
 $node->safe_psql('postgres', "CREATE EXTENSION aqo");
 $node->safe_psql('postgres', "ALTER SYSTEM SET aqo.mode = 'intelligent'");
 $node->command_ok([ 'pgbench', '-i', '-s', '1' ], 'init pgbench tables');
-$node->command_ok([ 'pgbench', '-t', "1000", '-c', "20", '-j', "20" ],
+$node->command_ok([ 'pgbench', '-t', "1000", '-c', "10", '-j', "10" ],
 	'pgbench in intelligent mode');
 
 $node->safe_psql('postgres', "ALTER SYSTEM SET aqo.mode = 'controlled'");
-$node->command_ok([ 'pgbench', '-t', "1000", '-c', "20", '-j', "20" ],
+$node->command_ok([ 'pgbench', '-t', "1000", '-c', "10", '-j', "10" ],
 	'pgbench in controlled mode');
 
 $node->safe_psql('postgres', "ALTER SYSTEM SET aqo.mode = 'disabled'");
-$node->command_ok([ 'pgbench', '-t', "1000", '-c', "20", '-j', "20" ],
+$node->command_ok([ 'pgbench', '-t', "1000", '-c', "10", '-j', "10" ],
 	'pgbench in disabled mode');
 
 $node->safe_psql('postgres', "DROP EXTENSION aqo");
 $node->safe_psql('postgres', "CREATE EXTENSION aqo");
 
 $node->safe_psql('postgres', "ALTER SYSTEM SET aqo.mode = 'learn'");
-$node->command_ok([ 'pgbench', '-t', "1000", '-c', "20", '-j', "20" ],
+$node->command_ok([ 'pgbench', '-t', "1000", '-c', "10", '-j', "10" ],
 	'pgbench in learn mode');
 
 $node->safe_psql('postgres', "ALTER SYSTEM SET aqo.mode = 'frozen'");
-$node->command_ok([ 'pgbench', '-t', "1000", '-c', "20", '-j', "20" ],
+$node->command_ok([ 'pgbench', '-t', "1000", '-c', "10", '-j', "10" ],
 	'pgbench in frozen mode');
 
 $node->safe_psql('postgres', "DROP EXTENSION aqo");
