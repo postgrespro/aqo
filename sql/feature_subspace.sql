599fff9b5fe7ada63b08f63102e4d20244b2fb6f
Alena Rybakina <a.rybakina@postgrespro.ru>
2022-08-03 20:14:22 +0300

    Fix feature_subspace output test. Delete platform dependent lines containing Memory and add order by command in feature_subspace test for statical result.

--- sql/feature_subspace.sql
+++ sql/feature_subspace.sql
@@ -10,20 +10,35 @@ CREATE TABLE a AS (SELECT gs AS x FROM generate_series(1,10) AS gs);
 CREATE TABLE b AS (SELECT gs AS x FROM generate_series(1,100) AS gs);
 
 --
+-- Returns string-by-string explain of a query. Made for removing some strings
+-- from the explain output.
+--
+CREATE OR REPLACE FUNCTION expln(query_string text) RETURNS SETOF text AS $$
+BEGIN
+    RETURN QUERY
+        EXECUTE format('EXPLAIN (ANALYZE, COSTS OFF, TIMING OFF, SUMMARY OFF) %s', query_string);
+    RETURN;
+END;
+$$ LANGUAGE PLPGSQL;
+
+--
 -- A LEFT JOIN B isn't equal B LEFT JOIN A.
 --
-
-EXPLAIN (ANALYZE, COSTS OFF, TIMING OFF, SUMMARY OFF)
-SELECT * FROM a LEFT JOIN b USING (x);
+SELECT str AS result
+FROM expln('
+SELECT * FROM a LEFT JOIN b USING (x);') AS str
+WHERE str NOT LIKE '%Memory%';
 
 -- TODO: Using method of other classes neighbours we get a bad estimation.
-EXPLAIN (ANALYZE, COSTS OFF, TIMING OFF, SUMMARY OFF)
-SELECT * FROM b LEFT JOIN a USING (x);
+SELECT str AS result
+FROM expln('
+SELECT * FROM b LEFT JOIN a USING (x);') AS str
+WHERE str NOT LIKE '%Memory%';
 
 -- Look into the reason: two JOINs from different classes have the same FSS.
 SELECT to_char(d1.targets[1], 'FM999.00') AS target FROM aqo_data d1
 JOIN aqo_data d2 ON (d1.fs <> d2.fs AND d1.fss = d2.fss)
-WHERE 'a'::regclass = ANY (d1.oids) AND 'b'::regclass = ANY (d1.oids);
+WHERE 'a'::regclass = ANY (d1.oids) AND 'b'::regclass = ANY (d1.oids) order by target;
 
 DROP TABLE a,b CASCADE;
 SELECT true FROM aqo_reset();
