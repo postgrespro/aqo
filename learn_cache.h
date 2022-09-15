253599a98ae3c07af0ab466a957a6ca10c3365d6
a.pervushina <a.pervushina@postgrespro.ru>
2022-09-15 11:53:36 +0300

    Cumulative commit on the 'learn on statement timeout' feature. Now it works quite stable, merge it into master branch

--- learn_cache.h
+++ learn_cache.h
@@ -7,13 +7,13 @@
 
 extern bool aqo_learn_statement_timeout;
 
-extern void lc_init(void);
 extern bool lc_update_fss(uint64 fhash, int fsshash, OkNNrdata *data,
 						  List *relids);
 extern bool lc_has_fss(uint64 fhash, int fss);
 extern bool lc_load_fss(uint64 fhash, int fsshash, OkNNrdata *data,
 						List **relids);
 extern void lc_remove_fss(uint64 fhash, int fss_hash);
+extern void lc_flush_data(void);
 extern void lc_assign_hook(bool newval, void *extra);
 
 #endif /* LEARN_CACHE_H */
