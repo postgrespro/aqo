e25e579f282d14bebfa82f4e1029a20352d7b834
Daniil Anisimov <d.anisimov@postgrespro.ru>
2022-12-29 14:30:23 +0700

    Remove the learn_cache routine because non-transactional storage is now used.

--- aqo_shared.h
+++ aqo_shared.h
@@ -9,23 +9,9 @@
 
 #define AQO_SHARED_MAGIC	0x053163
 
-typedef struct
-{
-	/* XXX we assume this struct contains no padding bytes */
-	uint64	fs;
-	int64	fss;
-} htab_key;
-
-typedef struct
-{
-	htab_key	key;
-	uint32		hdr_off; /* offset of data in DSM cache */
-} htab_entry;
-
 typedef struct AQOSharedState
 {
 	LWLock		lock;			/* mutual exclusion */
-	dsm_handle	dsm_handler;
 
 	/* Storage fields */
 	LWLock		stat_lock; /* lock for access to stat storage */
@@ -47,16 +33,11 @@ typedef struct AQOSharedState
 
 extern shmem_startup_hook_type prev_shmem_startup_hook;
 extern AQOSharedState *aqo_state;
-extern HTAB *fss_htab;
 
 extern int fs_max_items; /* Max number of feature spaces that AQO can operate */
 extern int fss_max_items;
 
 extern Size aqo_memsize(void);
-extern void reset_dsm_cache(void);
-extern void *get_dsm_all(uint32 *size);
-extern char *get_cache_address(void);
-extern uint32 get_dsm_cache_pos(uint32 size);
 extern void aqo_init_shmem(void);
 
 #endif /* AQO_SHARED_H */
