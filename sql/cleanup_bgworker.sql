60c87d40839eb143ece917e82ccc9d9979cdf4be
Andrey Lepikhov <a.lepikhov@postgrespro.ru>
2023-01-31 11:26:32 +0500

    Add wait function for background cleanup procedure. It makes the cleanup_bgworker regression test much more stable: don't forget, that bgworker process needs time to start and make clean operations. We may do even better if will invent some cleanup tag and checking function.

--- sql/cleanup_bgworker.sql
+++ sql/cleanup_bgworker.sql
@@ -9,9 +9,32 @@ CREATE INDEX aqo_test0_idx_a ON aqo_test0 (a);
 ANALYZE aqo_test0;
 
 CREATE EXTENSION aqo;
+SELECT true FROM aqo_reset();
 SET aqo.join_threshold = 0;
 SET aqo.mode = 'learn';
 SET aqo.cleanup_bgworker = 'true';
+
+CREATE OR REPLACE FUNCTION wait_bgworker(value int, timeout_ms int)
+RETURNS void
+AS $$
+DECLARE
+   cnt      int;
+   endtime  timestamp;
+   curtime  timestamp;
+BEGIN
+   SELECT  now() + make_interval(secs := timeout_ms * 1000) INTO endtime;
+   SELECT count(*) FROM aqo_queries INTO cnt;
+   WHILE (cnt <> value) LOOP
+      SELECT count(*) FROM aqo_queries INTO cnt;
+      SELECT now() INTO curtime;
+      IF (curtime > endtime) THEN
+         raise EXCEPTION 'Wait inturrupted because of timeout';
+         EXIT;
+      END IF;
+   END LOOP;
+   raise NOTICE 'Successful AQO cleanup of bgworker process';
+END;
+$$ LANGUAGE PLPGSQL VOLATILE;
 
 EXPLAIN SELECT t1.a, t2.b FROM aqo_test0 AS t1, aqo_test0 AS t2
 WHERE t1.a < 1 AND t1.b < 1 AND t2.c < 1 AND t2.d < 1;
@@ -40,7 +63,7 @@ AS (
 CREATE INDEX aqo_test1_idx_a ON aqo_test1 (a);
 ANALYZE aqo_test1;
 
-SELECT count(*) FROM aqo_queries;
+SELECT wait_bgworker(1, 100);
 
 EXPLAIN SELECT * FROM aqo_test0
 WHERE a < 3 AND b < 3 AND c < 3 AND d < 3;
@@ -49,7 +72,7 @@ EXPLAIN SELECT * FROM aqo_test1
 WHERE a < 4 AND b < 4;
 
 -- Test2: Must delete 1 out of 2 entries
-SELECT count(*) FROM aqo_queries;
+SELECT wait_bgworker(3, 100);
 DROP TABLE aqo_test1;
 
 CREATE TABLE IF NOT EXISTS aqo_test1(a int, b int);
@@ -62,7 +85,7 @@ AS (
 CREATE INDEX aqo_test1_idx_a ON aqo_test1 (a);
 ANALYZE aqo_test1;
 
-SELECT count(*) FROM aqo_queries;
+SELECT wait_bgworker(1, 100);
 
 EXPLAIN SELECT * FROM aqo_test0
 WHERE a < 3 AND b < 3 AND c < 3 AND d < 3;
@@ -71,10 +94,11 @@ EXPLAIN SELECT * FROM aqo_test1
 WHERE a < 4 AND b < 4;
 
 -- Test3: delete 2 out of 2 entries
-SELECT count(*) FROM aqo_queries;
+SELECT wait_bgworker(3, 100);
 DROP TABLE aqo_test0;
 DROP TABLE aqo_test1;
-SELECT count(*) FROM aqo_queries;
+
+SELECT wait_bgworker(1, 100);
 
 SET aqo.cleanup_bgworker = 'false';
 DROP EXTENSION aqo;
